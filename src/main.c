a6b238c8edcf1c85affabba8b26931db2a31dd01
Tommaso <tommy.terribili@gmail.com>
2022-10-27 20:35:35 +0200

    handled CTRL+D signal now minishell is quitting properly

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tterribi <tterribi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tterribi <tterribi@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:52:50 by tterribi          #+#    #+#             */
-/*   Updated: 2022/10/27 12:52:18 by tterribi         ###   ########.fr       */
+/*   Updated: 2022/10/27 20:33:59 by tterribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int		exit = 0;
-	char	*input;
+	char	*buff;
 	int i = 0;
 	struct s_env *env_head = NULL;
 
@@ -23,11 +23,12 @@ int	main(int argc, char **argv, char **envp)
 
 	while (exit != 1)
 	{
-		input = readline("Minishell> ");
-		input = parse(input);
-		if (ft_strcmp(input, "luce") == 0)
+		buff = readline("Minishell> ");
+		if (!buff)
+			return (printf("\nMinishell> exit\n"));
+		if (ft_strcmp(buff, "luce") == 0)
 			printf("DUCE\n");
 		else
-			printf("%s\n", input);
+			printf("%s\n", buff);
 	}
 }
